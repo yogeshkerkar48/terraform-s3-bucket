5317a8693170e75a5ca2c5e54905380d305688ea
yogeshkerkar48 <56548865+yogeshkerkar48@users.noreply.github.com>
2025-08-05 13:03:38 +0530

    Add files via upload

---
+++ lambdainvokingetl.py
@@ -0,0 +1,29 @@
+
+from awsglue.context import GlueContext
+from pyspark.context import SparkContext
+import sys
+from awsglue.utils import getResolvedOptions
+from pyspark.sql import SparkSession
+
+# Get arguments passed from Lambda or Glue
+#args = getResolvedOptions(sys.argv, ['S3_Source_file', 'S3_Destination_file'])
+
+source_path = "s3://trans123321/abcd/"
+destination_path = "s3://newtransform123456/data/"
+# Create Spark and Glue Context
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+# Read CSV from S3
+df = spark.read.csv(source_path, inferSchema=True, header=True)
+
+# Drop unwanted columns
+columns_to_drop = ["community_area", "ward"]
+df = df.drop(*columns_to_drop)
+
+# Coalesce to a single output file
+df_single = df.coalesce(1)
+
+# Write back to S3 in CSV format
+df_single.write.mode("overwrite").option("header", "true").csv(destination_path)
